e7b4f98daac285c3c4e77a5849e82c20e5c26aba
tr1ppyb0y <asatishobhit9@gmail.com>
2023-05-12 18:33:02 +0530

    1. Added custom validation on a model field. 2. Created custom model field and added it to UserAccount model.

--- post/models.py
+++ post/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
+from django.core.exceptions import ValidationError
+
+
+BAD_WORDS = ['java', 'waterfall', 'enterprise']
+def validate_no_bad_words(content):
+    if any([word in content.lower() for word in BAD_WORDS]):
+        raise ValidationError('This post contains bad words!')
 
 
 class Post(models.Model):
     author = models.ForeignKey(User, null=True, blank=True, on_delete=CASCADE)
-    content = models.CharField(max_length=140)
+    content = models.CharField(max_length=140, validators=[validate_no_bad_words])
     created_on = models.DateTimeField('date created', auto_now_add=True)
 
     def __str__(self):
--- useraccount/models.py
+++ useraccount/models.py
@@ -1,6 +1,49 @@
+from typing import Any
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
+from django.core.exceptions import ValidationError
+
+# C0FFEE -> 12648430
+def hex_to_int(value):
+    if value is None:
+        return
+    return int(value, 16)
+
+
+def int_to_hex(value):
+    hex_val = format(value, 'X')
+    # Pad with 0's if needed
+    hex_val = '0'*(6-len(hex_val)) + hex_val
+    return hex_val
+
+# For more sophisticated example,
+# see how ImageField is implemented.
+class RBGcolorField(models.CharField):
+    description = 'A field for holding RGB color values.'
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        self._validators = []
+        self.validators.append(self.validate_all_values_hex)
+        super().__init__(*args, **kwargs)
+
+    def validate_all_values_hex(self, value):
+        try:
+            hex_to_int(value)
+        except:
+            raise ValidationError(f'{value} is not a hex value.')
+    
+    # Check connection to see what kind 
+    # of database project is working with.
+    def db_type(self, connection: Any) -> str:
+        return 'UNSIGNED INTEGER(3)'
+    
+    def from_db_value(self, value, expression, connection):
+        if value is None:
+            return
+        return int_to_hex(value)
+    
+    def get_prep_value(self, value: Any) -> Any:
+        return hex_to_int(value)
 
 class UserAccount(models.Model):
     user = models.ForeignKey(User, null=False, blank=False, on_delete=CASCADE)
@@ -9,6 +52,7 @@ class UserAccount(models.Model):
     created_on = models.DateTimeField('date created', auto_now_add=True)
     birthday = models.DateField()
     location = models.CharField(max_length=200)
+    favorite_color = RBGcolorField(null=True, max_length=6)
     about = models.TextField(null=True)
 
     def name(self):
