56b5768a29e35594217cafd666b031020028c47c
tr1ppyb0y <asatishobhit9@gmail.com>
2023-05-13 14:27:30 +0530

    Using Generic relations to map User and Business to same Profile model.

--- profilepage/models.py
+++ profilepage/models.py
@@ -1,15 +1,10 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.db.models.deletion import CASCADE
-from django.apps import apps
-from useraccount.models import UserAccount
-from business.models import Business
-
-from django.contrib.contenttypes.models import ContentType
-
 
 
 class ProfilePage(models.Model):
-    user_account = models.ForeignKey(UserAccount, null=True, blank=False, on_delete=CASCADE)
-
-    def __str__(self):
-        return '{}'.format(self.user_account.first_name)
+    content_type = models.ForeignKey(ContentType, on_delete=CASCADE, null=True)
+    object_id = models.PositiveIntegerField(null=True)
+    subject = GenericForeignKey('content_type', 'object_id')
